aaa14bbe2b7f0ff31054d9af3fa08a2ea24f6e62
Ying Chan <89020187+ychan2020@users.noreply.github.com>
2022-05-29 18:08:56 +0800

    Pylint tested
    Pylint tested and remove remember me

--- auth.py
+++ auth.py
@@ -1,4 +1,6 @@
-# auth.py
+"""
+auth.py
+"""
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -8,47 +10,55 @@ from . import db
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
+    """Render login page"""
     return render_template('login.html')
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
+    """User login"""
     email = request.form.get('email')
     password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
 
     user = User.query.filter_by(email=email).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
-    if not user or not check_password_hash(user.password, password): 
+    if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))  # Reload page if user not exist or wrong password
 
     # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
+    login_user(user)
     return redirect(url_for('main.profile'))
+
 
 @auth.route('/signup')
 def signup():
+    """Render Signup page"""
     return render_template('signup.html')
+
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
-
+    """User Signup"""
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(
+        email=email).first()  # if this returns a user, then the email already exists in database
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again  
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name,
+                    password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
     db.session.add(new_user)
@@ -56,8 +66,10 @@ def signup_post():
 
     return redirect(url_for('auth.login'))
 
+
 @auth.route('/logout')
 @login_required
 def logout():
+    """Logout and Redirect to main page"""
     logout_user()
     return redirect(url_for('main.index'))
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
-# models.py
+"""
+models.py
+"""
 
 from flask_login import UserMixin
 from . import db
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    """Database fields connection"""
+    id = db.Column(db.Integer, primary_key=True)  # primary key is required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    role = db.Column(db.String(100))
