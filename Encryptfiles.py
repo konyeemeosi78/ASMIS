45d83e81792ed88dd2508ee5ee7c09544531f977
Jonathan Callaghan <jcallaghan84@googlemail.com>
2022-05-30 15:50:13 +0800

    Create Encryptfiles.py

---
+++ Encryptfiles.py
@@ -0,0 +1,65 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+
+    return open("key.key", "rb").read()
+
+def encrypt(filename, key):
+
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+
+        file_data = file.read()
+
+    encrypted_data = f.encrypt(file_data)
+
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+
+def decrypt(filename, key):
+
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Simple File Encryptor Script")
+    parser.add_argument("file", help="File to encrypt/decrypt")
+    parser.add_argument("-g", "--generate-key", dest="generate_key", action="store_true",
+                        help="Whether to generate a new key or use existing")
+    parser.add_argument("-e", "--encrypt", action="store_true",
+                        help="Whether to encrypt the file, only -e or -d can be specified.")
+    parser.add_argument("-d", "--decrypt", action="store_true",
+                        help="Whether to decrypt the file, only -e or -d can be specified.")
+
+    args = parser.parse_args()
+    file = args.file
+    generate_key = args.generate_key
+
+    if generate_key:
+        write_key()
+    # load the key
+    key = load_key()
+
+    encrypt_ = args.encrypt
+    decrypt_ = args.decrypt
+
+    if encrypt_ and decrypt_:
+        raise TypeError("Please specify whether you want to encrypt the file or decrypt it.")
+    elif encrypt_:
+        encrypt(file, key)
+    elif decrypt_:
+        decrypt(file, key)
+    else:
+        raise TypeError("Please specify whether you want to encrypt the file or decrypt it.")
