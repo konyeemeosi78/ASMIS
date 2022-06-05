b73f2eb3bb422584e20269f934de7eab00df7a38
nitinsk07 <94173851+nitinsk07@users.noreply.github.com>
2022-06-05 18:29:10 +0400

    Update models.py

--- admin/models.py
+++ admin/models.py
@@ -1,8 +1,12 @@
 """
 models.py
 """
+import base64
+import os
 
+import onetimepass             
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash                                                
 from . import db
 
 
@@ -13,3 +17,28 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     role = db.Column(db.String(100))
+    otp_secret = db.Column(db.String(16))
+
+    # add additional field for storing the otp_secret
+
+    def __init__(self, **kwargs):
+        # generating otp secret for the function
+        super(User, self).__init__(**kwargs)
+        if self.otp_secret is None:
+            # generate a random secret
+            self.otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
+
+    def verify_password(self, password):
+        """verify function for the checking password"""
+        return check_password_hash(self.password_hash, password)
+
+    def get_totp_uri(self):
+        """function to create otp url"""
+        return 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
+            .format(self.name, self.otp_secret)
+
+    @staticmethod
+    def verify_totp(token, otp_secret):
+        """verify customer provided input ot the system secret code"""
+        return onetimepass.valid_totp(token, otp_secret)
+                                    
